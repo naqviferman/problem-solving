fc6e4e3e6705bf001c7878b8faa6fcaea07e999e
Ferman Naqvi <naqviferman@gmail.com>
2021-02-28 02:05:17 +0530

    Added test cases for TrailingZeroesInFactorial

--- src/main/java/mathproblems/TrailingZeroesInFactorial.java
+++ src/main/java/mathproblems/TrailingZeroesInFactorial.java
@@ -1,5 +1,14 @@
 package mathproblems;
 
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
 public class TrailingZeroesInFactorial {
     /*
     For an integer N find the number of trailing zeroes in N!.
@@ -11,9 +20,15 @@ public class TrailingZeroesInFactorial {
     5! = 120 so the number of trailing zero is 1.
     */
 
-    public static void main(String[] args) {
-        int input = 4617;
-        System.out.println(trailingZeroes(input));
+//    public static void main(String[] args) {
+//        int input = 4617;
+//        System.out.println(trailingZeroes(input));
+//    }
+
+    @TestFactory
+    public Stream<DynamicTest> testTrailingZeroes() {
+        int[][] input = new int[][]{{1, 1, 5}, {2, 24, 101}, {3, 249, 1000}, {4, 1151, 4617}};
+        return Arrays.stream(input).map(entry -> dynamicTest("Test_" + entry[0], () -> assertEquals(entry[1], trailingZeroes(entry[2]))));
     }
 
     static int trailingZeroes(int N) {
